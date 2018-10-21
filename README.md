240c0a5937f81f7ebcd031ad7cd79f45229669d6
kallookoo <747817+kallookoo@users.noreply.github.com>
2018-10-21 12:46:33 +0200

    Add support to register_uninstall_hook

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # wp_plugin_check_admin_referer
 
-Checks if using the plugin on register_activation_hook or register_deactivation_hook
+Checks if using the plugin on register_activation_hook, register_deactivation_hook and register_uninstall_hook
 
 Use check_admin_referer to avoid security exploits
--- src/wp-plugin-check-admin-referer.php
+++ src/wp-plugin-check-admin-referer.php
@@ -4,36 +4,64 @@
  *
  * @author Sergio (kallookoo) <sergio@dsergio.com>
  * @license https://www.gnu.org/licenses/gpl-2.0.html GPL2 or later
- * @version 1.1.1
+ * @version 1.2
  * @package dsergio\com\WordPress\Plugin\Helpers
  */
 
 if ( ! function_exists( 'wp_plugin_check_admin_referer' ) ) {
 	/**
-	 * Checks if using the plugin on register_activation_hook or register_deactivation_hook
-	 * Use check_admin_referer to avoid security exploits
+	 * Checks if using the plugin on register_activation_hook, register_deactivation_hook and register_uninstall_hook
+	 *
+	 * Note: Use check_admin_referer to avoid security exploits
 	 *
 	 * @param  string $file Plugin filename.
 	 *
 	 * @return bool
 	 */
 	function wp_plugin_check_admin_referer( $file ) {
-		if ( is_admin() && current_user_can( 'activate_plugins' ) ) {
+		if ( is_admin() ) {
 			global $action;
 
-			$current_action = (string) $action;
+			$actions = array(
+				'single'   => array( 'activate', 'deactivate' ),
+				'multiple' => array( 'activate-selected', 'deactivate-selected', 'delete-selected' ),
+			);
 
-			if ( 'activate' === $current_action || 'deactivate' === $current_action ) {
-				$plugin = ( ! empty( $_REQUEST['plugin'] ) ) ? (string) $_REQUEST['plugin'] : ''; // WPCS: CSRF ok, sanitization ok.
-				return check_admin_referer( "{$current_action}-plugin_{$plugin}" );
+			if ( ! empty( $action ) && is_string( $action ) ) {
+				$cap = ( 'delete-selected' === $action ) ? 'delete_plugins' : 'activate_plugins';
+				if ( current_user_can( $cap ) ) {
+					if ( in_array( $action, $actions['single'], true ) ) {
+						$plugin = ( ! empty( $_REQUEST['plugin'] ) ) ? (string) $_REQUEST['plugin'] : ''; // WPCS: CSRF ok, sanitization ok.
+						return check_admin_referer( "{$action}-plugin_{$plugin}" );
 
-			} elseif ( 'activate-selected' === $current_action || 'deactivate-selected' === $current_action ) {
-				check_admin_referer( 'bulk-plugins' );
+					} elseif ( in_array( $action, $actions['multiple'], true ) ) {
+						if ( false !== check_admin_referer( 'bulk-plugins' ) ) {
+							$plugin = plugin_basename( trim( $file ) );
 
-				$plugin  = plugin_basename( trim( $file ) );
-				$plugins = ( ! empty( $_POST['checked'] ) ) ? (array) $_POST['checked'] : array(); // WPCS: sanitization ok.
+							/**
+							 * Get correct global
+							 *
+							 * If delete-selected action use $_REQUEST otherwise, POST is used
+							 *
+							 * Note from /wp-admin/plugins.php for delete-selected action
+							 * $_POST = from the plugin form;
+							 * $_GET  = from the FTP details screen.
+							 *
+							 * @var array
+							 */
+							$global = ( 'delete-selected' === $action ) ? $_REQUEST : $_POST;
 
-				return in_array( $plugin, $plugins, true );
+							/**
+							 * Checked plugins
+							 *
+							 * @var array
+							 */
+							$plugins = isset( $global['checked'] ) ? (array) wp_unslash( $global['checked'] ) : array();
+
+							return in_array( $plugin, $plugins, true );
+						}
+					}
+				}
 			}
 		}
 
